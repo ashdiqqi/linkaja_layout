f5c2d72522d7d7de20489adaddffa5b62cf33d13
= <=>
2024-10-23 22:11:58 +0700

    halaman history

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import './widget/second_section.dart';
 import './widget/third_section.dart';
 import './widget/four_section.dart';
 import './widget/five_section.dart';
+import './widget/history_page.dart'; // Import halaman baru History
 
 void main() {
   runApp(const MyApp());
@@ -17,48 +18,78 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('LinkAja'),
-          backgroundColor: const Color.fromARGB(255, 224, 29, 15),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType
-              .fixed, // Menambahkan tipe fixed agar semua item terlihat
-          selectedItemColor: Colors.black, // Warna item yang dipilih
-          unselectedItemColor: Colors.black, // Warna item yang tidak dipilih
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.history),
-              label: 'History',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.paypal),
-              label: 'Pay',
-            ),
-            BottomNavigationBarItem(
-              // Menu Profile dengan warna hitam
-              icon: Icon(Icons.person),
-              label: 'Profile',
-            ),
-          ],
-        ),
-        body: Container(
-          color: const Color(0xFFF5F7F8),
-          child: ListView(
-            padding: const EdgeInsets.all(15),
-            children: [
-              TopSection(),
-              const SecondSection(),
-              ThirdSection(),
-              FourSection(),
-              FiveSection(),
-            ],
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    if (index == 1) {
+      // Jika menu History dipilih, navigasi ke halaman History
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const HistoryPage()),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('LinkAja'),
+        backgroundColor: const Color.fromARGB(255, 224, 29, 15),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black,
+        currentIndex: _selectedIndex, // Mengontrol item yang dipilih
+        onTap: _onItemTapped, // Menangani logika saat item diklik
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
           ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'History',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.paypal),
+            label: 'Pay',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+      body: Container(
+        color: const Color(0xFFF5F7F8),
+        child: ListView(
+          padding: const EdgeInsets.all(15),
+          children: [
+            TopSection(),
+            const SecondSection(),
+            ThirdSection(),
+            FourSection(),
+            FiveSection(),
+          ],
         ),
       ),
     );
